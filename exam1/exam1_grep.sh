7865eb6a8bdfc6dd302f689dedc3c56674439937
MelissaPunguil <melissa.punguil@est.ikiam.edu.ec>
2022-06-10 16:28:59 -0500

    CAmbio en exam para hacer la parte 2

--- exam1/exam1_grep.sh
+++ exam1/exam1_grep.sh
@@ -1,2 +1,14 @@
-#! /bin/bash
-cat ../data/interleukin.txt
+#! /bin/bas
+#Parte 2
+$1 = ../data/interleukin.txt
+$|1
+touch Grep/mouse.txt Grep/human.txt Grep/linear_rna.txt
+  
+grep "mouse" $1 > ../Grep/mouse.txt
+cat mouse.txt
+grep "human" $1 > ../Grep/human.txt
+cat human.txt
+grep "linear mRNA" $1 > ../Grep/linear_rna.txt
+sort -n linear_rna.txt
+cat linear_rna.txt
+
